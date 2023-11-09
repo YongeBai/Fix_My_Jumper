1df9f7389696e9979995fb34796b9592c24a2fe8
YongeBai <yongebai@gmail.com>
2023-11-09 21:49:54 +0800

    improved shot tracking, started STT. Need to decide when to measure angles

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 import numpy as np
 from flask import Flask, Response, render_template
 from mediapipe.framework.formats import landmark_pb2
+import time
 
 model_path = "./pose_landmarker_lite.task"
 
@@ -13,29 +14,8 @@ LANDMARKS = [
     ("Right Index Finger", 20),
 ]
 
-
-def print_result(
-    result: mp.tasks.vision.PoseLandmarkerResult,
-    output_image: mp.Image,
-    timestamp_ms: int,
-):
-    try:
-        if result.pose_landmarks:
-            landmarks = result.pose_landmarks[0]
-            for _, idx in LANDMARKS:
-                if landmarks[idx].visibility < 0.5:
-                    return
-                if landmarks[idx].presence < 0.5:
-                    return
-            if (
-                landmarks[12].x < landmarks[14].x
-                and landmarks[14].x < landmarks[16].x
-                and landmarks[16].x < landmarks[20].x
-            ):
-                print("Shot")
-
-    except:
-        pass
+prev_shot_time = 0
+total_shots = 0
 
 
 def draw_landmarks(
@@ -47,23 +27,16 @@ def draw_landmarks(
     pose_landmarks = pose_landmarks_list[0]
 
     pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend(
-        [
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z)
-            for landmark in pose_landmarks
-        ]
-    )
-    # for _, idx in LANDMARKS:
-    #     pose_landmarks_proto.landmark.append(
-    #         landmark_pb2.NormalizedLandmark(
-    #             x=pose_landmarks[idx].x,
-    #             y=pose_landmarks[idx].y,
-    #             z=pose_landmarks[idx].z,
-    #         )
-    #     )
-    connections = set([(12, 14), (14, 16), (16, 20)])
-    print(pose_landmarks_proto)
+
+    for _, idx in LANDMARKS:
+        pose_landmarks_proto.landmark.append(
+            landmark_pb2.NormalizedLandmark(
+                x=pose_landmarks[idx].x,
+                y=pose_landmarks[idx].y,
+                z=pose_landmarks[idx].z,
+            )
+        )
+    connections = set([(0, 1), (1, 2), (2, 3)])
     mp.solutions.drawing_utils.draw_landmarks(
         annotated_image,
         pose_landmarks_proto,
@@ -74,6 +47,34 @@ def draw_landmarks(
     return annotated_image
 
 
+def count_shots(detection_result: mp.tasks.vision.PoseLandmarkerResult) -> int:
+    global prev_shot_time
+
+    current_time = time.time()
+    shots = 0
+    if current_time > prev_shot_time + 5:
+        try:
+            pose_landmarks_list = detection_result.pose_landmarks
+            pose_landmarks = pose_landmarks_list[0]
+            if (
+                pose_landmarks[20].visibility < 0.7
+            ):
+                return shots
+            if (
+                pose_landmarks[12].y < pose_landmarks[14].y
+                and pose_landmarks[14].y < pose_landmarks[16].y
+                and pose_landmarks[16].y < pose_landmarks[20].y
+            ):
+                # TODO:ADD ANGLE CALCULATION HERE AND FIGURE OUT WHEN U WANT TO MEASURE THE ANGLES
+                print("Shot")
+                shots += 1
+                prev_shot_time = time.time()
+        except IndexError:
+            pass
+
+    return shots
+
+
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
 PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
@@ -82,6 +83,9 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 options = PoseLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.VIDEO,
+    min_pose_detection_confidence = 0.7,
+    min_pose_presence_confidence = 0.7,
+    min_tracking_confidence = 0.7,
 )
 
 app = Flask(__name__)
@@ -90,6 +94,8 @@ camera = cv.VideoCapture(0)
 
 
 def generate_frames():
+    global total_shots
+
     with PoseLandmarker.create_from_options(options) as landmarker:
         while True:
             success, frame = camera.read()
@@ -104,12 +110,19 @@ def generate_frames():
 
             results = landmarker.detect_for_video(mp_image, frame_timestamp_ms)
 
+            # track shots
+            shot = count_shots(results)
+            if shot:
+                total_shots += shot
+                print(total_shots)
+
+            # draw landmarks
             frame.flags.writeable = True
             frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
             if results.pose_landmarks:
                 frame = draw_landmarks(frame, results)
 
-            ret, buffer = cv.imencode(".jpg", frame)
+            _, buffer = cv.imencode(".jpg", frame)
             frame = buffer.tobytes()
             yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
--- shot_tracking.py
+++ shot_tracking.py
@@ -1,2 +1,57 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from transformers.pipelines.audio_utils import ffmpeg_microphone_live
 
 
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "distil-whisper/distil-medium.en"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+transcriber = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+
+def transcribe(chunk_length_s=20.0, stream_chunk_s=1.0):
+    sampling_rate = transcriber.feature_extractor.sampling_rate
+
+    mic = ffmpeg_microphone_live(
+        sampling_rate=sampling_rate,
+        chunk_length_s=chunk_length_s,
+        stream_chunk_s=stream_chunk_s,
+    )
+
+    print("Recording...")
+    for item in transcriber(mic, generate_kwargs={"max_new_tokens": 128}):
+        yield item["text"]
+
+
+def track_shots():
+    total_shots = 0
+    makes = 0
+    while True:
+        for transcriptions in transcribe():
+            print(transcriptions)
+            if ("fucker" in transcriptions.lower() 
+                or "fuck" in transcriptions.lower() 
+                or "fucking" in transcriptions.lower()):
+                makes += 1
+            total_shots += 1
+            print(f"{makes}/{total_shots}")
+
+
+track_shots()
